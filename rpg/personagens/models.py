dffdecd09306041e4fb3f40ff5425209e2f73f7e
João Mário Bulla Oliveira <jmbullaoliveira@gmail.com>
2026-01-21 10:18:05 -0300

    serializers atualizados

--- rpg/personagens/models.py
+++ rpg/personagens/models.py
@@ -8,6 +8,7 @@ class Personagem(models.Model):
     nome = models.CharField(max_length=100)
     level = models.IntegerField()
     xp = models.IntegerField(default=0)
+    # Classes app needed
     #class1 = models.ForeignKey()
     #class2 = models.ForeignKey()
     backstory = models.CharField(max_length=10000, blank=True)
